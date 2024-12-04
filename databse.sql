c7d227ccc4ffe6c1d73f9213d5e93ce13bfb2e9b
unknown <PedrooTz>
2024-12-04 18:31:27 -0300

    script banco

--- databse.sql
+++ databse.sql
@@ -1,44 +1,32 @@
 CREATE SCHEMA db_transporta_web;
-
 USE db_transporta_web;
+
+SELECT m.*
+FROM tbl_motorista m
+LEFT JOIN tbl_equipe e ON m.id = e.id_motorista
+WHERE e.id_motorista IS NULL;
 
 CREATE TABLE tbl_motorista(
 
 id int primary key auto_increment not null,
-
 nome varchar (150) not null,
-
 data_nascimento date not null,
-
 cpf varchar (14) not null,
-
 telefone varchar(13) not null,
-
 cnh varchar(20) not null,
-
 foto_url text not null,
-
 email varchar (100) not null,
-
 senha varchar (32) not null,
-
 disponibilidade_status boolean not null
-
 );
 
 INSERT INTO tbl_motorista (nome, data_nascimento, cpf, telefone, cnh, foto_url, email, senha, disponibilidade_status)
-
 VALUES
-
-('João Silva', '1984-04-21', '47496775821', '11959747737', ' 12345678901', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS97h5PsMDsjpHqK3_jSbKU2OS1VMQVsKYAzg&s', 'joao.silva@example.com', 'senha123', TRUE),
-
-('Leonardo Silva', '1972-04-21', '74796775821', '11988459010', '98765432109', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS97h5PsMDsjpHqK3_jSbKU2OS1VMQVsKYAzg&s', 'leonardo.silva@example.com', 'leonardo123', TRUE),
-
-('Luana Freire', '2000-01-12', '59596775821', '11959655434', '10293847562', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS97h5PsMDsjpHqK3_jSbKU2OS1VMQVsKYAzg&s', 'luana.freire@example.com', 'luana123', FALSE),
-
-('Guilherme Oito', '1945-01-29', '48596775873', '11765655434', '89193847562', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS97h5PsMDsjpHqK3_jSbKU2OS1VMQVsKYAzg&s', 'guilherme.oito@example.com', 'guilherme123', FALSE),
-
-('Yasmin Quatro', '1999-02-11', '12111175821', '11959651111', '27654847562', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS97h5PsMDsjpHqK3_jSbKU2OS1VMQVsKYAzg&s', 'yasmin.quatro@example.com', 'yasmin123', FALSE);
+('João Silva', '1984-04-21', '47496775821', '11959747737', '12345678901', 'https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zm90byUyMGRvJTIwcGVyZmlsfGVufDB8fDB8fHww', 'joao.silva@example.com', 'senha123', TRUE),
+('Leonardo Silva', '1972-04-21', '74796775821', '11988459010', '98765432109', 'https://st3.depositphotos.com/1857171/19538/i/450/depositphotos_195381282-stock-photo-bearded-strong-pride-handsome-man.jpg', 'leonardo.silva@example.com', 'leonardo123', TRUE),
+('Luana Freire', '2000-01-12', '59596775821', '11959655434', '10293847562', 'https://www.negociossc.com.br/wp-content/uploads/2024/03/Anotacao-2024-03-06-091727.png', 'luana.freire@example.com', 'luana123', FALSE),
+('Guilherme Oito', '1945-01-29', '48596775873', '11765655434', '89193847562', 'https://st4.depositphotos.com/3450477/27636/i/450/depositphotos_276361582-stock-photo-scared-emotional-surprised-male-model.jpg', 'guilherme.oito@example.com', 'guilherme123', FALSE),
+('Yasmin Quatro', '1999-02-11', '12111175821', '11959651111', '27654847562', 'https://blog.mercadaodosoculos.com.br/wp-content/uploads/2022/08/mulheres-empreendedoras.jpg', 'yasmin.quatro@example.com', 'yasmin123', FALSE);
 
 CREATE TABLE tbl_empresa(
 
@@ -62,29 +50,27 @@ senha varchar (32) not null
 
 );
 
-drop table tbl_empresa;
-
 INSERT INTO tbl_empresa (nome, razaoSocial, cep, cnpj, numero_telefone, foto_url, email, senha)
 
 VALUES
 
-('Empresa Alpha', 'Alpha Ltda', '12345-600', '12345678000190', '11987654321', 'https://img.freepik.com/vetores-gratis/vetor-de-gradiente-de-logotipo-colorido-de-passaro_343694-1365.jpg', 'contato@alpha.com', 'senhaAlpha123'),
+('Translog Expresso', 'Translog Transportes e Logística Ltda', '12345-600', '12345678000190', '11985904322', 'https://img.freepik.com/vetores-premium/vector-de-logotipo-de-logistica-e-transporte-minimalista-design-moderno-e-elegante-de-alta-resolucao_1273060-16.jpg?semt=ais_hybrid', 'contato@translog.com', 'senhaTranslog123'),
 
-('Empresa Beta', 'Beta S/A', '12345-200', '22345678000190', '11987654322', 'https://img.freepik.com/vetores-gratis/vetor-de-gradiente-de-logotipo-colorido-de-passaro_343694-1365.jpg', 'contato@beta.com', 'senhaBeta123'),
+('MoviLog', 'MoviLog Soluções Logísticas S/A', '12345-200', '22345678000190', '11987654322', 'https://images.vexels.com/content/142887/preview/growing-logistic-company-logo-2f366b.png', 'contato@movilog.com', 'senhaMoviLog123'),
 
-('Empresa Gamma', 'Gamma Comércio', '12336-100', '32345678000190', '11987654323', 'https://img.freepik.com/vetores-gratis/vetor-de-gradiente-de-logotipo-colorido-de-passaro_343694-1365.jpg', 'contato@gamma.com', 'senhaGamma123'),
+('FastCargo', 'FastCargo Logística Integrada Ltda', '12336-100', '32345678000190', '11987654323', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQINCAeBJ4LfUMca2dKs2kuBxOy3PcZWwXBCA&s', 'contato@fastcargo.com', 'senhaFastCargo123'),
 
-('Empresa Delta', 'Delta Tecnologia', '23456-600', '42345678000190', '11987654324', 'https://img.freepik.com/vetores-gratis/vetor-de-gradiente-de-logotipo-colorido-de-passaro_343694-1365.jpg', 'contato@delta.com', 'senhaDelta123'),
+('RotaLog', 'RotaLog Transportes e Distribuição', '23456-600', '42345678000190', '11987654324', 'https://i.pinimg.com/originals/5f/de/fb/5fdefb40c803c71b03f1fc1c10a2415c.jpg', 'contato@rotalog.com', 'senhaRotaLog123'),
 
-('Empresa Épsilon', 'Épsilon Industrial', '32145-600', '52345678000190', '11987654325', 'https://img.freepik.com/vetores-gratis/vetor-de-gradiente-de-logotipo-colorido-de-passaro_343694-1365.jpg', 'contato@epsilon.com', 'senhaEpsilon123'),
+('LogiBrasil', 'LogiBrasil Logística e Distribuição Ltda', '32145-600', '52345678000190', '11987654325', 'https://www.shutterstock.com/image-vector/truck-logo-vector-260nw-445725301.jpg', 'contato@logibrasil.com', 'senhaLogiBrasil123'),
 
-('Empresa Zeta', 'Zeta Serviços', '54321-600', '62345678000190', '11987654326', 'https://img.freepik.com/vetores-gratis/vetor-de-gradiente-de-logotipo-colorido-de-passaro_343694-1365.jpg', 'contato@zeta.com', 'senhaZeta123'),
+('MaxTransporte', 'MaxTransporte Logística Internacional', '54321-600', '62345678000190', '11987654326', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdlk_36dik5q14YQdEv1oIwzO2bRET3-UhKA&s', 'contato@maxtransporte.com', 'senhaMaxTransporte123'),
 
-('Empresa Eta', 'Eta Ltda', '12392-600', '72345678000190', '11987654327', 'https://img.freepik.com/vetores-gratis/vetor-de-gradiente-de-logotipo-colorido-de-passaro_343694-1365.jpg', 'contato@eta.com', 'senhaEta123'),
+('GlobalLog', 'GlobalLog Soluções Logísticas Ltda', '12392-600', '72345678000190', '11987654327', 'https://png.pngtree.com/png-clipart/20200727/original/pngtree-trailer-transport-service-logo-design-png-image_5069943.jpg', 'contato@globallog.com', 'senhaGlobalLog123'),
 
-('Empresa Theta', 'Theta Comércio', '12332-600', '82345678000190', '11987654328', 'https://img.freepik.com/vetores-gratis/vetor-de-gradiente-de-logotipo-colorido-de-passaro_343694-1365.jpg', 'contato@theta.com', 'senhaTheta123'),
+('UniCargo', 'UniCargo Transportes e Logística S/A', '12332-600', '82345678000190', '11987654328', 'https://static.vecteezy.com/ti/vetor-gratis/p1/7617669-caminhao-transporte-veiculo-sombra-logo-vetor.jpg', 'contato@unicargo.com', 'senhaUniCargo123'),
 
-('Empresa Iota', 'Iota Consultoria', '54321-450', '92345678000190', '11987654329', 'https://img.freepik.com/vetores-gratis/vetor-de-gradiente-de-logotipo-colorido-de-passaro_343694-1365.jpg', 'contato@iota.com', 'senhaIota123');
+('VelozLog', 'VelozLog Transporte e Logística Ltda', '54321-450', '92345678000190', '11987654329', 'https://i.pinimg.com/736x/fb/d5/7e/fbd57e8f42dc18984a80fb78db7007a3.jpg', 'contato@velozlog.com', 'senhaVelozLog123');
 
 CREATE TABLE tbl_avaliacao (
 
@@ -280,6 +266,7 @@ VALUES
 ('712-939-VCD', '2024-10-15', '15:30:00', NULL, 'Empresa Épsilon', 'Cliente E', false, 5, 5, 5, 5, 5, 5);
 
 SELECT
+    v.id,
     v.id_viagem,
     v.dia_partida,
     v.horario_partida,
@@ -297,13 +284,15 @@ FROM
     tbl_viagem v
 INNER JOIN tbl_partida p ON v.id_partida = p.id
 INNER JOIN tbl_destino d ON v.id_destino = d.id
-INNER JOIN tbl_motorista m ON v.id_motorista = m.id
+LEFT JOIN tbl_motorista m ON v.id_motorista = m.id -- Alterado para LEFT JOIN
 INNER JOIN tbl_veiculo veiculo ON v.id_veiculo = veiculo.id
 INNER JOIN tbl_tipo_carga tipo_carga ON v.id_tipo_carga = tipo_carga.id
 INNER JOIN tbl_empresa empresa ON v.id_empresa = empresa.id
 ORDER BY
     v.id DESC;
 
+    
+    select * from tbl_viagem;
 
 CREATE TABLE tbl_tipo_carga(
 
@@ -371,8 +360,6 @@ VALUES
 
 ('Equipamentos eletrônicos de alto valor', 120.25, 5);
 
-drop table tbl_equipe;
-
 CREATE TABLE tbl_equipe (
 id int primary key auto_increment not null,
 id_motorista int not null,
@@ -388,14 +375,12 @@ insert into tbl_equipe (id_motorista, id_empresa) values
 (2,2),
 (3,3),
 (4,4),
-(5,5),
-(21,1);
+(5,5);
 
 SELECT m.nome AS nome_motorista
 FROM tbl_equipe te
 JOIN tbl_motorista m ON te.id_motorista = m.id
 WHERE te.id_empresa = 1;  -- Aqui você usa o id da empresa logada
-
 
 
 CREATE TABLE tbl_postagem (
